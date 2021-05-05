e89e81e42bbbca5ff8ce5e4365921d4a6cb48688
Neotoxic-off <paul.gardien@epitech.eu>
2021-05-06 01:36:26 +0200

    version 1.0.0

---
+++ Alter Ego.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace FDR
+{
+    partial class Alter_Ego
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browse = new System.Windows.Forms.Button();
+            this.directory = new System.Windows.Forms.FolderBrowserDialog();
+            this.remove = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.status = new System.Windows.Forms.RichTextBox();
+            this.only_images = new System.Windows.Forms.CheckBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.auto_remove = new System.Windows.Forms.CheckBox();
+            this.only_videos = new System.Windows.Forms.CheckBox();
+            this.only_music = new System.Windows.Forms.CheckBox();
+            this.dark_theme = new System.Windows.Forms.CheckBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.cancel = new System.Windows.Forms.Button();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.percent = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // browse
+            // 
+            this.browse.BackColor = System.Drawing.Color.Transparent;
+            this.browse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.browse.Location = new System.Drawing.Point(401, 535);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(81, 23);
+            this.browse.TabIndex = 0;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = false;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // remove
+            // 
+            this.remove.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.remove.Location = new System.Drawing.Point(6, 19);
+            this.remove.Name = "remove";
+            this.remove.ReadOnly = true;
+            this.remove.Size = new System.Drawing.Size(451, 93);
+            this.remove.TabIndex = 2;
+            this.remove.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.remove);
+            this.groupBox2.Location = new System.Drawing.Point(12, 136);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(470, 118);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " Duplicate ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.logs);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Size = new System.Drawing.Size(470, 118);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Status ";
+            // 
+            // logs
+            // 
+            this.logs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logs.Location = new System.Drawing.Point(6, 26);
+            this.logs.Name = "logs";
+            this.logs.ReadOnly = true;
+            this.logs.Size = new System.Drawing.Size(451, 79);
+            this.logs.TabIndex = 1;
+            this.logs.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.status);
+            this.groupBox3.Location = new System.Drawing.Point(12, 260);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox3.Size = new System.Drawing.Size(470, 110);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = " Logs ";
+            // 
+            // status
+            // 
+            this.status.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.status.Location = new System.Drawing.Point(6, 26);
+            this.status.Name = "status";
+            this.status.ReadOnly = true;
+            this.status.Size = new System.Drawing.Size(451, 79);
+            this.status.TabIndex = 1;
+            this.status.Text = "";
+            // 
+            // only_images
+            // 
+            this.only_images.AutoSize = true;
+            this.only_images.Location = new System.Drawing.Point(13, 26);
+            this.only_images.Name = "only_images";
+            this.only_images.Size = new System.Drawing.Size(84, 17);
+            this.only_images.TabIndex = 5;
+            this.only_images.Text = "Only Images";
+            this.only_images.UseVisualStyleBackColor = true;
+            this.only_images.CheckedChanged += new System.EventHandler(this.only_images_CheckedChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.auto_remove);
+            this.groupBox4.Controls.Add(this.only_videos);
+            this.groupBox4.Controls.Add(this.only_music);
+            this.groupBox4.Controls.Add(this.only_images);
+            this.groupBox4.Location = new System.Drawing.Point(12, 376);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox4.Size = new System.Drawing.Size(306, 76);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = " Search Options ";
+            // 
+            // auto_remove
+            // 
+            this.auto_remove.AutoSize = true;
+            this.auto_remove.Location = new System.Drawing.Point(131, 26);
+            this.auto_remove.Name = "auto_remove";
+            this.auto_remove.Size = new System.Drawing.Size(91, 17);
+            this.auto_remove.TabIndex = 8;
+            this.auto_remove.Text = "Auto Remove";
+            this.auto_remove.UseVisualStyleBackColor = true;
+            this.auto_remove.CheckedChanged += new System.EventHandler(this.auto_remove_CheckedChanged);
+            // 
+            // only_videos
+            // 
+            this.only_videos.AutoSize = true;
+            this.only_videos.Location = new System.Drawing.Point(131, 49);
+            this.only_videos.Name = "only_videos";
+            this.only_videos.Size = new System.Drawing.Size(82, 17);
+            this.only_videos.TabIndex = 7;
+            this.only_videos.Text = "Only Videos";
+            this.only_videos.UseVisualStyleBackColor = true;
+            this.only_videos.CheckedChanged += new System.EventHandler(this.only_videos_CheckedChanged);
+            // 
+            // only_music
+            // 
+            this.only_music.AutoSize = true;
+            this.only_music.Location = new System.Drawing.Point(13, 49);
+            this.only_music.Name = "only_music";
+            this.only_music.Size = new System.Drawing.Size(78, 17);
+            this.only_music.TabIndex = 6;
+            this.only_music.Text = "Only Music";
+            this.only_music.UseVisualStyleBackColor = true;
+            this.only_music.CheckedChanged += new System.EventHandler(this.only_music_CheckedChanged);
+            // 
+            // dark_theme
+            // 
+            this.dark_theme.AutoSize = true;
+            this.dark_theme.Location = new System.Drawing.Point(13, 26);
+            this.dark_theme.Name = "dark_theme";
+            this.dark_theme.Size = new System.Drawing.Size(85, 17);
+            this.dark_theme.TabIndex = 6;
+            this.dark_theme.Text = "Dark Theme";
+            this.dark_theme.UseVisualStyleBackColor = true;
+            this.dark_theme.CheckedChanged += new System.EventHandler(this.dark_theme_CheckedChanged);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.dark_theme);
+            this.groupBox5.Location = new System.Drawing.Point(324, 376);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox5.Size = new System.Drawing.Size(158, 76);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = " Interface ";
+            // 
+            // cancel
+            // 
+            this.cancel.BackColor = System.Drawing.Color.Transparent;
+            this.cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancel.Location = new System.Drawing.Point(314, 535);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(81, 23);
+            this.cancel.TabIndex = 7;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = false;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // progress
+            // 
+            this.progress.ForeColor = System.Drawing.Color.Red;
+            this.progress.Location = new System.Drawing.Point(16, 28);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(367, 13);
+            this.progress.TabIndex = 8;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.percent);
+            this.groupBox6.Controls.Add(this.progress);
+            this.groupBox6.Location = new System.Drawing.Point(12, 458);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(470, 60);
+            this.groupBox6.TabIndex = 9;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = " Progress ";
+            // 
+            // percent
+            // 
+            this.percent.AutoSize = true;
+            this.percent.Location = new System.Drawing.Point(386, 28);
+            this.percent.Name = "percent";
+            this.percent.Size = new System.Drawing.Size(38, 13);
+            this.percent.TabIndex = 9;
+            this.percent.Text = "Ready";
+            // 
+            // Alter_Ego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 570);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.browse);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Alter_Ego";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alter Ego";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.FolderBrowserDialog directory;
+        private System.Windows.Forms.RichTextBox remove;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox status;
+        private System.Windows.Forms.CheckBox only_images;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.CheckBox dark_theme;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.CheckBox only_music;
+        private System.Windows.Forms.CheckBox only_videos;
+        private System.Windows.Forms.CheckBox auto_remove;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label percent;
+    }
+}
+
---
+++ Alter Ego.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FDR
+{
+    public partial class Alter_Ego : Form
+    {
+        List<string> to_remove = new List<string>();
+        public Alter_Ego()
+        {
+            InitializeComponent();
+        }
+
+        private int count_char(string str, char c)
+        {
+            int total = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                    total++;
+            }
+            return (total);
+        }
+
+        private string crop(string message)
+        {
+            if (count_char(message, '\\') >= 3)
+            {
+                return ($"{message.Split('\\')[0]}\\{message.Split('\\')[1]}\\...\\{message.Split('\\')[count_char(message, '\\')]}");
+            }
+
+            return (message);
+        }
+
+        private int UILogs(string message)
+        {
+            logs.Invoke(new MethodInvoker(delegate
+            {
+                logs.Text = message;
+                logs.Refresh();
+            }));
+
+            return (0);
+        }
+
+        private int display(string message)
+        {
+            status.Invoke(new MethodInvoker(delegate
+            {
+                DateTime date = DateTime.Now;
+                string result = date.ToString("hh:mm:ss tt");
+                status.AppendText($"{result} | {message}\n");
+                status.SelectionStart = status.Text.Length;
+                status.ScrollToCaret();
+                status.Refresh();
+            }));
+            
+            return (0);
+        }
+
+        private int add_to_trash(string message)
+        {
+            remove.Invoke(new MethodInvoker(delegate
+            {
+                remove.AppendText($"{message}\n");
+                remove.SelectionStart = remove.Text.Length;
+                remove.ScrollToCaret();
+                remove.Refresh();
+            }));
+           
+            return (0);
+        }
+
+        private int get_folder()
+        {
+            directory.ShowDialog();
+            if (directory.SelectedPath.Length == 0)
+                return (-1);
+
+            return (0);
+        }
+
+        private int check_ext(string file)
+        {
+            string[] filter_image = { ".png", ".jpg", ".jpeg", ".gif" };
+            string[] filter_music = { ".mp3", ".wav", ".flac", ".riff", ".bwf", ".ogg", ".aiff" };
+            string[] filter_videos = { ".mp4", ".webm", ".mkv", ".flv", ".vob", ".gif", ".avi", ".m4v", ".flv" };
+
+            if (only_images.Checked == true)
+            {
+                for (int i = 0; i < filter_image.Length; i++)
+                {
+                    if (file.EndsWith(filter_image[i]))
+                        return (1);
+                }
+
+                return (0);
+            }
+
+            if (only_music.Checked == true)
+            {
+                for (int i = 0; i < filter_music.Length; i++)
+                {
+                    if (file.EndsWith(filter_music[i]))
+                        return (1);
+                }
+
+                return (0);
+            }
+
+            if (only_videos.Checked == true)
+            {
+                for (int i = 0; i < filter_videos.Length; i++)
+                {
+                    if (file.EndsWith(filter_videos[i]))
+                        return (1);
+                }
+
+                return (0);
+            }
+
+            return (1);
+        }
+
+        private bool are_equal(byte[] s1, byte[] s2)
+        {
+            return (s1.SequenceEqual(s2));
+        }
+
+        private async Task<Task> purge_all()
+        {
+            for (int i = 0; i < to_remove.Count; i++)
+            {
+                if (File.Exists(to_remove[i]) == true)
+                {
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                    File.Delete(to_remove[i]);
+                }
+            }
+            to_remove = null;
+            return (Task.CompletedTask);
+        }
+
+        private async Task<Task> check_all()
+        {
+            int value = 0;
+            int duplicate = 0;
+            string[] files = Directory.GetFiles(directory.SelectedPath, "*.*", SearchOption.AllDirectories);
+            int size = files.Length;
+            MD5 md5 = MD5.Create();
+            byte[] original = null;
+            byte[] compared = null;
+
+            display($"Starting scan: {size} files");
+
+            for (int i = 0; i < size; i++)
+            {
+                groupBox2.Invoke(new MethodInvoker(delegate
+                {
+                    groupBox2.Text = $" Duplicate: {duplicate} ";
+                }));
+
+                if (check_ext(files[i]) == 1)
+                {
+                    for (int j = i + 1; j < size && to_remove.Contains(files[j]) == false; j++)
+                    {
+                        if (check_ext(files[j]) == 1)
+                        {
+                            original = md5.ComputeHash(File.OpenRead(files[i]));
+                            compared = md5.ComputeHash(File.OpenRead(files[j]));
+
+                            if (original.Length == compared.Length)
+                            {
+                                if (are_equal(original, compared) == true)
+                                {
+                                    to_remove.Add(files[j]);
+                                    add_to_trash($"{crop(files[j])}");
+                                    duplicate++;
+                                }
+                                else
+                                {
+                                    UILogs($"{crop(files[j])}");
+                                }
+                            }
+                        }
+                    }
+                }
+
+                progress.Invoke(new MethodInvoker(delegate
+                {
+                    value = ((i * 100) / files.Length);
+                    progress.Value = value;
+                    percent.Text = $"{value} %";
+                }));
+            }
+            progress.Invoke(new MethodInvoker(delegate
+            {
+                value = 0;
+                progress.Value = value;
+                percent.Text = $"Scan complete";
+            }));
+
+            display($"Scan complete: {duplicate} duplicate");
+            return (Task.CompletedTask);
+        }
+
+        private async void browse_Click(object sender, EventArgs e)
+        {
+            Task check_task = null;
+            Task clean_task = null;
+
+            if (get_folder() == 0)
+            {
+                remove.Clear();
+                status.Clear();
+                logs.Clear();
+
+                display($"Directory set {crop(directory.SelectedPath)}");
+                check_task = await Task.Run(() => check_all());
+                check_task.Wait();
+                if (auto_remove.Checked == true)
+                {
+                    clean_task = await Task.Run(() => purge_all());
+                    clean_task.Wait();
+                }
+
+                return;
+            }
+            display($"Directory not set");
+        }
+
+        private async Task<Task> change_theme()
+        {
+            if (dark_theme.Checked == true)
+            {
+                BackColor = Color.FromArgb(35, 39, 42);
+                groupBox1.ForeColor = Color.White;
+                groupBox2.ForeColor = Color.White;
+                groupBox3.ForeColor = Color.White;
+                groupBox4.ForeColor = Color.White;
+                groupBox5.ForeColor = Color.White;
+                groupBox6.ForeColor = Color.White;
+
+                logs.BackColor = Color.FromArgb(44, 47, 51);
+                logs.ForeColor = Color.White;
+
+                remove.BackColor = Color.FromArgb(44, 47, 51);
+                remove.ForeColor = Color.White;
+
+                status.BackColor = Color.FromArgb(44, 47, 51);
+                status.ForeColor = Color.White;
+
+                browse.ForeColor = Color.White;
+                cancel.ForeColor = Color.White;
+
+                percent.ForeColor = Color.White;
+            } else {
+                BackColor = SystemColors.Control;
+                groupBox1.ForeColor = SystemColors.ControlText;
+                groupBox2.ForeColor = SystemColors.ControlText;
+                groupBox3.ForeColor = SystemColors.ControlText;
+                groupBox4.ForeColor = SystemColors.ControlText;
+                groupBox5.ForeColor = SystemColors.ControlText;
+                groupBox6.ForeColor = SystemColors.ControlText;
+
+                logs.BackColor = SystemColors.Control;
+                logs.ForeColor = SystemColors.WindowText;
+
+                remove.BackColor = SystemColors.Control;
+                remove.ForeColor = SystemColors.WindowText;
+
+                status.BackColor = SystemColors.Control;
+                status.ForeColor = SystemColors.WindowText;
+
+                browse.ForeColor = SystemColors.ControlText;
+                cancel.ForeColor = SystemColors.ControlText;
+
+                percent.ForeColor = SystemColors.ControlText;
+
+            }
+
+            return (Task.CompletedTask);
+        }
+
+        private void only_images_CheckedChanged(object sender, EventArgs e)
+        {
+            if (only_images.Checked == true)
+            {
+                only_music.Checked = false;
+                only_videos.Checked = false;
+                display($"Only images set");
+            }
+            else
+                display($"Only images unset");
+        }
+
+        private void dark_theme_CheckedChanged(object sender, EventArgs e)
+        {
+            if (dark_theme.Checked == true)
+                display("Dark theme set");
+            else
+                display("Dark theme unset");
+            change_theme().Wait();
+        }
+
+        private void auto_remove_CheckedChanged(object sender, EventArgs e)
+        {
+            if (auto_remove.Checked == true)
+                display($"Auto remove set");
+            else
+                display($"Auto remove unset");
+        }
+
+        private void only_music_CheckedChanged(object sender, EventArgs e)
+        {
+            if (only_music.Checked == true)
+            {
+                only_images.Checked = false;
+                only_videos.Checked = false;
+                display("Only music set");
+            }
+            else
+                display("Only music unset");
+        }
+
+        private void only_videos_CheckedChanged(object sender, EventArgs e)
+        {
+            if (only_videos.Checked == true)
+            {
+                only_images.Checked = false;
+                only_music.Checked = false;
+                display("Only videos set");
+            }
+            else
+                display("Only videos unset");
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+    }
+}
